f12f3c1a0a368cd9e2be6b4968ab28267df548b1
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-08-01 23:48:36 -0500

    [Feature] Django structure for api

--- star_wars_api/settings.py
+++ star_wars_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'graphene_django',  # This will also make the `graphql_schema` management command available
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'star_wars_api.schema.schema'
+}
--- star_wars_api/urls.py
+++ star_wars_api/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# for graphql
+from django.contrib import admin
+from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
