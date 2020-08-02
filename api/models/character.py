e8457377466587f5bcf06bf921f4f92d2f31130d
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-08-02 03:11:28 -0500

    [Feature] Cambio en las estructuras de los modelos y eliminación de restricciones de no nulidad para algunos campos.

--- api/models/character.py
+++ api/models/character.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Character(models.Model):
     name = models.CharField(max_length=100)
-    height = models.IntegerField()
-    mass = models.IntegerField()
-    gender = models.CharField(max_length=50)
+    height = models.IntegerField(null=True)
+    mass = models.IntegerField(null=True)
+    gender = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.name
--- api/models/film.py
+++ api/models/film.py
@@ -5,10 +5,10 @@ from .planet import Planet
 
 class Film(models.Model):
     name = models.CharField(max_length=100)
-    episode_id = models.IntegerField()
-    opening_crawl = models.CharField(max_length=1200)
-    director = models.CharField(max_length=120)
-    producer = models.CharField(max_length=120)
+    episode_id = models.IntegerField(null=True)
+    opening_crawl = models.CharField(max_length=1200, null=True)
+    director = models.CharField(max_length=120, null=True)
+    producer = models.CharField(max_length=120, null=True)
     characters = models.ManyToManyField(Character)
     planets = models.ManyToManyField(Planet)
 
